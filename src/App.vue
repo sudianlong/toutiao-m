f2ffe81621a3cf18dd73e0ba7016a957adec49bc
苏殿龙 <2907712399@qq.com>
2020-11-15 18:17:28 +0800

    引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
     <h1>
       河马头条
     </h1>
+    <div>
+      <span class="icon-dice"></span>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 加载vant 的 核心组件库
+import Vant from 'vant'
+// 加载 vant 全局样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import './style/index.less'
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
